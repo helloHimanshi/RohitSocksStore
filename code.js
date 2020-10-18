48d66d54ee7bf6559eecc47655505f5d30924cd9
Himanshi Tiwari <tiwari.himanshi09@gmail.com>
2020-10-18 13:57:05 +0530

    Initial commit

---
+++ code.js
@@ -0,0 +1,30 @@
+let n = 17;
+let ar = [];
+
+function rohitSocksStore(n, ar) {
+  var i, j;
+  var pair = 0;
+  var count = 0;
+
+  for (i = 0; i < n; i++) {
+    var arr = prompt("Enter the socks number.");
+    ar.push(arr);
+  }
+  alert(ar);
+  console.log(ar);
+  ar.sort();
+  console.log("Sorted Socks Array : " + ar);
+
+  for (i = 0; i < ar.length; i++) {
+    count = 1;
+    for (j = i + 1; j < ar.length; j++) {
+      if (ar[i] == ar[j]) {
+        count++;
+      }
+    }
+    if (count % 2 == 0) {
+      pair++;
+    }
+    return pair;
+  }
+  console.log("Total numbers of matching socks pairs are : " + rohitSocksStore(n, ar))
